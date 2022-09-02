f4d0ebc61ba4f7f011af56c2bcaf8742dec42e90
Jeremiah <jeremiah@openai.com>
2022-09-02 09:50:07 -0700

    Image generation support demo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { App, LogLevel, SocketModeReceiver } = require('@slack/bolt');
 require('dotenv').config();
 const { Configuration, OpenAIApi } = require("openai");
+const axios = require('axios');
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -20,12 +21,12 @@ const app = new App({
     console.log('⚡️ Hello Whirled Peas... Bolt app is running!\n[Socket Mode = ENABLED]');
 })();
 
-// Listen to the slash command, and when received, respond with a ephemeral message including the user who tirgerred the command
+// Listen to the slash command, and when received, respond with a ephemeral message including the user who triggered the command
 app.command('/openai', async ({ command, ack, payload, context, respond }) => {
     ack();
     console.log(`lab1 Responding to `, payload.user_id);
 
-    // add in loading logic for UX
+    // TODO - add in loading logic for UX
 
     try {
         const response = await openai.createCompletion("text-davinci-002", {
@@ -42,6 +43,71 @@ app.command('/openai', async ({ command, ack, payload, context, respond }) => {
     } catch (error) {
         console.log('error');
         console.log(error);
+    }
+
+ });
+
+ // Listen to the slash command, and when received, respond with a ephemeral message including the user who triggered the command
+ // Then take payload text to send in API call to DALL-E2 API, format and send the response back to the user in Slack with their generated image
+app.command('/openai-dalle2', async ({ command, ack, payload, context, respond, say }) => {
+    ack();
+    console.log(`[openai-dalle2] Responding to Slack user id: `, payload.user_id);
+    try {
+        // const prompt = "An Impressionist oil painting of someone riding on a psychedelic train going through a tunnel on a mountainside in a desert filled with wildflowers and cacti. There is a hawk flying in the sky and there is an orange sun in the sky at sunset.";
+        const prompt = payload.text;
+        const loading = await app.client.chat.postEphemeral({
+            channel : payload.channel_id,
+            user    : payload.user_id,
+            text    : `:rocket: Sending this prompt to DALL-E2:\n> _${prompt}_`
+        });
+        const res = await axios.post('https://api.openai.com/v1/images/generations', {
+            model           : "image-alpha-001",
+            caption         : prompt,
+            num_images      : 1, // TODO - support full range of options [up to 10]
+            size            : "256x256", // TODO - support full range of sizes [64x64, 256x256 or 1024x1024]
+            response_format : "url" // TODO - support both options [or b64_json]
+        }, {
+            headers: {
+            'Content-Type'  : 'application/json',
+            'Authorization' : 'Bearer ' + process.env.OPENAI_API_KEY
+            }
+        });
+        console.log('\n[openai-dalle2] OpenAI DALL-E2 API response url:');
+        const generation = res.data.data[0].url; // TODO - add logic to support more than 1 url
+
+        // https://slack.dev/bolt-js/concepts#message-sending with blocks via .say() as method from Slack's Bolt framework
+        await say({
+            // blocks mocked up via Slack's "Block Kit Builder"
+            blocks: [
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": ":art: Powered by <https://openai.com/dall-e-2/|DALL-E2> :art:"
+                    }
+                },
+                {
+                    "type": "image",
+                    "title": {
+                        "type": "plain_text",
+                        "text": "An Impressionist oil painting of someone riding on a psychedelic train going through a tunnel on a mountainside in a desert filled with wildflowers and cacti. There is a hawk flying in the sky and there is an orange sun in the sky at sunset.",
+                        "emoji": true
+                    },
+                    "image_url": generation, // Slack API requires file size < 2 MB in size + public url
+                    "alt_text": "Image generated via DALL-E2 API from OpenAI"
+                }
+            ]
+          });
+    } catch (error) {
+        console.log(`\n[openai-dalle2] error with code: ${error.code}, status: ${error.response.status}, and statusText: ${error.response.statusText}.\n Full error output below:\n${error}`);
+        await app.client.chat.postEphemeral({
+            channel : payload.channel_id,
+            user    : payload.user_id,
+            text    : `API Error\n>HTTP status code: *${error.response.status}*\n>Message: *${error.response.statusText}*`
+        });
     }
 
  });
